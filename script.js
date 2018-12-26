545769445950514026f333de2afb2972db3df2ce
Sah <saumil.sah@sap.com>
2018-12-26 19:50:11 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,61 @@
+/*var firstname = 'Saumil';
+console.log(firstname);
+*/
+
+/*var firstName = 'Saumil';   
+var lastName = 'Sah';
+
+var occupation, isMarried,
+    
+occupation = 'Footballer'; 
+isMarried = false;
+
+
+
+console.log(firstName +' '+ lastName + ' is a ' + occupation + '. Is he married? ' + isMarried);
+
+
+
+fisrtName = 'Leo';
+job = 'driver';
+
+alert(firstName +' '+ lastName + ' is a ' + occupation + '. Is he married? ' + isMarried);
+
+lastName = prompt('What is his last name?');
+
+console.log(firstName +' '+ lastName + ' is a ' + occupation + '. Is he married? ' + isMarried);
+*/
+
+/*var now;
+now = 2018
+ageSaumil = 27;
+ageSwapnil = 20;
+
+console.log(now * 2);
+console.log(now / 2);
+
+
+var isLarger = ageSaumil > ageSwapnil;
+
+console.log(isLarger);
+
+*/
+
+
+
+var markMass, markHeight, johnMass, johnHeight, markBmi,johnBmi;
+markMass = 65; //kg
+johnMass = 67.5; //mtr
+markHeight = 1.80;
+johnHeight = 1.85;
+markBmi = markMass / (markHeight * markHeight);
+johnBmi = johnMass / (johnHeight * johnHeight);
+
+console.log('Bmi of Mark is :' + ' ' + markBmi + 'and the BMI of John is: ' + ' ' + johnBmi);
+
+var greaterBmi = markBmi > johnBmi;
+
+console.log('Bmi of Mark\'s :' + ' ' + markBmi + ' and the BMI of John\'s: ' + ' ' + johnBmi + ' ' + 'Does Mark has a higher BMI than John?' + ' ' + greaterBmi);
+
+
+
